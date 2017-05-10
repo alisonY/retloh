037fd60eb7552444b73cff7fe68f3b06b7f61862
gaodong <895407214gao@sina.com>
2017-05-10 11:27:54 +0800

    集成ftpserver与client

---
+++ src/main/java/com/retloh/ftpserver/FtpServerUser.java
@@ -0,0 +1,89 @@
+package com.retloh.ftpserver;
+
+import java.util.List;  
+
+import org.apache.ftpserver.ftplet.Authority;  
+import org.apache.ftpserver.ftplet.AuthorizationRequest;  
+import org.apache.ftpserver.ftplet.User;  
+  
+/** 
+ * <һ�仰���ܼ���> 
+ * <������ϸ����>  
+ */  
+public class FtpServerUser implements User  
+{  
+    private String name;  
+      
+    private String password;  
+      
+    private String homeDirectory;
+    
+    private boolean writePermission;
+      
+    public void setName(String name)  
+    {  
+        this.name = name;  
+    }  
+      
+    public void setPassword(String password)  
+    {  
+        this.password = password;  
+    }  
+      
+    public void setHomeDirectory(String homeDirectory)  
+    {  
+        this.homeDirectory = homeDirectory;  
+    }  
+      
+    public AuthorizationRequest authorize(AuthorizationRequest arg0)  
+    {  
+        return null;  
+    }  
+      
+    public List<Authority> getAuthorities()  
+    {  
+        return null;  
+    }  
+      
+    public List<Authority> getAuthorities(Class<? extends Authority> arg0)  
+    {  
+        return null;  
+    }  
+      
+    public boolean getEnabled()  
+    {  
+        return true;  
+    }  
+      
+    public String getHomeDirectory()  
+    {  
+        return homeDirectory;  
+    }  
+      
+    public int getMaxIdleTime()  
+    {  
+        return 0;  
+    }  
+      
+    public String getName()  
+    {  
+        return name;  
+    }  
+      
+    public String getPassword()  
+    {  
+        return password;  
+    }  
+      
+    /** 
+     * @return ���� writepermission 
+     */  
+    public boolean getWritePermission()  
+    {  
+        return writePermission;  
+    }
+
+	public void setWritePermission(boolean writePermission) {
+		this.writePermission = writePermission;
+	}
+}  
