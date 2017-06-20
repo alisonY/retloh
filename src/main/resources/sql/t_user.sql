6612a53dd55a012f171b3dfc61585366e593af0f
gaodong <895407214gao@sina.com>
2017-06-20 14:19:07 +0800

    用户表添加组id

--- src/main/resources/sql/t_user.sql
+++ src/main/resources/sql/t_user.sql
@@ -8,6 +8,8 @@ CREATE TABLE `t_user` (
   `department` varchar(64) DEFAULT NULL COMMENT '部门',
   `user_rank` int(32) DEFAULT NULL COMMENT '等级',
   `user_type` int(32) DEFAULT NULL COMMENT '类型',
+  `groupid`  int(32) DEFAULT NULL COMMENT '组',
+  `group_desc`  varchar(64) DEFAULT NULL COMMENT '组描述',
   `update_time` datetime DEFAULT NULL COMMENT '修改时间',
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   `operator` varchar(64) DEFAULT NULL COMMENT '操作人',
